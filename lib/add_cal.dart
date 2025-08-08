b13f6fb7a5ff5fef50e38021fe8b4d69e1980056
Parth Sheth <parthsheth100@gmail.com>
2025-08-08 10:31:06 +0530

    - [X] Implement the addition of two numbers funcationality.

--- lib/add_cal.dart
+++ lib/add_cal.dart
@@ -1,3 +1,12 @@
 int add(String numbers) {
-  return 0;
+  //handles empty string or whitespace-only string
+  if (numbers.trim().isEmpty) return 0;
+  final parts = numbers.split(",");
+  var sum = 0;
+  for (final p in parts) {
+    final trimmed = p.trim();
+    if (trimmed.isEmpty) continue;
+    sum += int.parse(trimmed);
+  }
+  return sum;
 }
--- test/add_cal_test.dart
+++ test/add_cal_test.dart
@@ -2,7 +2,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:string_calculator/add_cal.dart';
 
 void main() {
+  //Test case when the input is empty string.
   test("returns 0 ", () {
     expect(add(''), 0);
+  });
+
+  // Test case when the input is single number.
+  test('returns the number itself for single number', () {
+    expect(add('1'), equals(1));
+  });
+
+  //Test case for two string input.
+  test('returns sum for two comma-separated numbers', () {
+    expect(add('1,2'), equals(3));
   });
 }
